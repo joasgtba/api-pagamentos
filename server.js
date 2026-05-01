161f4b4d6b3b33d5525694a00d6d7b51d014fd88
joasgtba <joasgtba@gmail.com>
2026-05-01 20:37:43 -0300

    API

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API Orquestrador rodando 🚀");
+});
+
+app.get("/health", (req, res) => {
+  res.json({ status: "ok" });
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
+});
