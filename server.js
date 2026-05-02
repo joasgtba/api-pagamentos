8dbf24c80fb193585ec2fd1fc84c9dfad335fc98
joasgtba <joasgtba@gmail.com>
2026-05-02 00:33:38 -0300

    Update server.js
    ..

--- server.js
+++ server.js
@@ -8,7 +8,17 @@ app.get("/", (req, res) => {
 app.get("/health", (req, res) => {
   res.json({ status: "ok" });
 });
+app.use(express.json());
 
+app.post("/pedido", (req, res) => {
+  const { valor, pedido_id } = req.body;
+
+  res.json({
+    message: "Pedido recebido",
+    pedido_id,
+    valor
+  });
+});
 app.listen(3000, () => {
   console.log("Servidor rodando na porta 3000");
 });
